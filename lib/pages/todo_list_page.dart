e7ef554400af800cd7c4e5bafe8065b5739d137d
Bruno César <bcgmeireles@gmail.com>
2022-08-04 22:05:49 -0300

    Criação do texto Tarefas Pendentes e botão Limpar Tudo

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -11,25 +11,53 @@ class TodoListPage extends StatelessWidget {
         body: Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Adicione uma tarefa",
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Adicione uma tarefa",
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(width: 7),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(14),
+                    primary: Color(0xff58D8B5),
+                  ),
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                )
+              ],
             ),
-            SizedBox(width: 7),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(14), primary: Color(0xff58D8B5)),
-              onPressed: () {},
-              child: Icon(
-                Icons.add,
-                size: 30,
-              ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Você possui 0 tarefas pendentes',
+                  ),
+                ),
+                SizedBox(width: 8),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff58D8B5),
+                    padding: EdgeInsets.all(14),
+                  ),
+                  onPressed: () {},
+                  child: Text('Limpar Tudo'),
+                ),
+              ],
             )
           ],
         ),
